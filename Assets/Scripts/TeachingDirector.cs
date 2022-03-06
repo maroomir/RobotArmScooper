dcdd7485faa665de9a93c8ff1fed6600faadedef
maroomir <maroomir@gmail.com>
2022-03-06 09:13:18 +0900

    Update the robot simulator

--- Assets/Scripts/TeachingDirector.cs
+++ Assets/Scripts/TeachingDirector.cs
@@ -10,10 +10,12 @@ using UnityEngine.UI;
 public class TeachingDirector : MonoBehaviour
 {
     public GameObject robot;
+    public GameObject gripper;
     public GameObject logger;
     public TrimMode trimMode;
 
     private RobotController _pRobotControl;
+    private GripperController _pGripperControl;
     private Dictionary<string, JointPoint> _pDicPoints;
     private TextMeshProUGUI _pDisplayLog;
     private int _nCount = 0;
@@ -21,10 +23,15 @@ public class TeachingDirector : MonoBehaviour
     
     private IEnumerator ReplayScript()
     {
+        _pDisplayLog.text = "Run Replay Script";
+        yield return new WaitForSeconds(1);
+        _pDisplayLog.text = "Home position";
         yield return HomeScript();
         foreach(JointPoint pPoint in _pDicPoints.Values)
         {
             yield return _pRobotControl.Move(pPoint);
+            if (pPoint.GripStatus != _pGripperControl.Status)
+                yield return _pGripperControl.Do(pPoint.GripStatus);
         }
     }
 
@@ -38,7 +45,9 @@ public class TeachingDirector : MonoBehaviour
     private void Start()
     {
         _pRobotControl = robot.GetComponent<RobotController>();
+        _pGripperControl = gripper.GetComponent<GripperController>();
         _pRobotControl.ControlMode = OperationMode.Teaching;
+        _pGripperControl.ControlMode = OperationMode.Teaching;
         _pDicPoints = new Dictionary<string, JointPoint>();
         _strTeachingRoot = Path.Combine(Directory.GetCurrentDirectory(), "Teaching");
         _pDisplayLog = logger.GetComponent<TextMeshProUGUI>();
@@ -65,33 +74,47 @@ public class TeachingDirector : MonoBehaviour
     };
 
     private PlayMode _eUpdateMode = PlayMode.Teach;
-    
+
     // Update is called on every frames
     private void Update()
     {
-        _eUpdateMode = CommonFactory.GetInputKeyDown(new[]
-                {KeyCode.Escape, KeyCode.Return, KeyCode.Space, KeyCode.Alpha0, KeyCode.H, KeyCode.R}) switch
-            {
-                KeyCode.Escape => PlayMode.Teach,
-                KeyCode.Return => PlayMode.Save,
-                KeyCode.Space => PlayMode.Trim,
-                KeyCode.Alpha0 => PlayMode.Home,
-                KeyCode.H => PlayMode.Home,
-                KeyCode.R => PlayMode.Replay,
-                _ => _eUpdateMode
-            };
-        
+        if (CommonFactory.GetInputKeys(new[] {KeyCode.LeftShift, KeyCode.RightShift}).Length > 0)
+        {
+            // Play the script with SHIFT key
+            _eUpdateMode = CommonFactory.GetInputKeyDown(new[]
+                    {KeyCode.H, KeyCode.R}) switch
+                {
+                    KeyCode.H => PlayMode.Home,
+                    KeyCode.R => PlayMode.Replay,
+                    _ => _eUpdateMode
+                };
+        }
+        else
+        {
+            _eUpdateMode = CommonFactory.GetInputKeyDown(new[]
+                    {KeyCode.Escape, KeyCode.Return, KeyCode.Space, KeyCode.Alpha0}) switch
+                {
+                    KeyCode.Escape => PlayMode.Teach,
+                    KeyCode.Return => PlayMode.Save,
+                    KeyCode.Space => PlayMode.Trim,
+                    KeyCode.Alpha0 => PlayMode.Home,
+                    _ => _eUpdateMode
+                };
+        }
+
         switch (_eUpdateMode)
         {
             case PlayMode.Wait:
                 break;
             case PlayMode.Teach:
                 _pRobotControl.ControlMode = OperationMode.Teaching;
+                _pGripperControl.ControlMode = OperationMode.Teaching;
                 _pDisplayLog.text = $"Teaching Mode [{_pRobotControl.JointPos.Print()}]";
                 break;
             case PlayMode.Save:
                 _pRobotControl.ControlMode = OperationMode.Auto;
                 JointPoint pPointSaved = _pRobotControl.JointPos;
+                pPointSaved.GripStatus = _pGripperControl.Status;
                 pPointSaved.Name = $"Pos{_nCount}";
                 _pDisplayLog.text = $"Save the {pPointSaved.Name} [{pPointSaved.Print()}]";
                 _pDicPoints.Add(pPointSaved.Name, pPointSaved);
@@ -103,6 +126,7 @@ public class TeachingDirector : MonoBehaviour
                 // Move to the redefined position
                 _pRobotControl.ControlMode = OperationMode.Auto;
                 JointPoint pPointTrimmed = _pRobotControl.JointPos;
+                pPointTrimmed.GripStatus = _pGripperControl.Status;
                 pPointTrimmed.Trim(trimMode);
                 _pDisplayLog.text = $"Move the trim position [{pPointTrimmed.Print()}]";
                 StartCoroutine(_pRobotControl.Move(pPointTrimmed));
@@ -111,6 +135,7 @@ public class TeachingDirector : MonoBehaviour
                 break;
             case PlayMode.Home:
                 _pRobotControl.ControlMode = OperationMode.Auto;
+                _pGripperControl.ControlMode = OperationMode.Auto;
                 _pDisplayLog.text = "Run Home Script";
                 StartCoroutine(HomeScript());
                 // Move the job flag to prevent overload
@@ -118,12 +143,12 @@ public class TeachingDirector : MonoBehaviour
                 break;
             case PlayMode.Replay:
                 _pRobotControl.ControlMode = OperationMode.Auto;
+                _pGripperControl.ControlMode = OperationMode.Auto;
                 if (_pDicPoints.Count > 0)
                 {
                     string strFilePath = Path.Combine(_strTeachingRoot, "Points.json");
                     TeachingFactory.SaveTeachingPoints(_pDicPoints, strFilePath);
                 }
-                _pDisplayLog.text = "Run Replay Script";
                 StartCoroutine(ReplayScript());
                 // Move the job flag to prevent overload
                 _eUpdateMode = PlayMode.Wait;
