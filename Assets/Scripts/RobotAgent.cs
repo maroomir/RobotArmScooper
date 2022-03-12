6c52b997ecfb0f4e51a1d638d7c6cac2a0657661
maroomir <maroomir@gmail.com>
2022-03-13 00:47:02 +0900

    Built in the basic codes

--- Assets/Scripts/RobotAgent.cs
+++ Assets/Scripts/RobotAgent.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
@@ -13,6 +15,14 @@ public class RobotAgent : Agent
 
     private RobotController _pRobotControl;
     private GripperController _pGripperControl;
+    private bool _bRunScript = false;
+    
+    private IEnumerator InitScript()
+    {
+        JointPoint pInitPos = JointPoint.FromPosition("Init", 135.0F, 0.0F, 90.0F, 0.0F, 90.0F, 0.0F, 0.0F);
+        pInitPos.FrameCount = 10;
+        yield return _pRobotControl.Move(pInitPos);
+    }
 
     // Initialize is called for ready to episode
     public override void Initialize()
@@ -20,25 +30,35 @@ public class RobotAgent : Agent
         base.Initialize();
         _pRobotControl = robot.GetComponent<RobotController>();
         _pGripperControl = robot.GetComponent<GripperController>();
+        _pRobotControl.ControlMode = OperationMode.Auto;
+        _pGripperControl.ControlMode = OperationMode.Auto;
     }
 
     // OnEpisodeBegin is called before the episode start
     public override void OnEpisodeBegin()
     {
         base.OnEpisodeBegin();
-        
+        Debug.Log("[AGENT] Episode Start");
+        StartCoroutine(InitScript());
     }
 
     // CollectObservations is collected the information for the policy update
     public override void CollectObservations(VectorSensor sensor)
     {
         base.CollectObservations(sensor);
+        sensor.AddObservation(_pRobotControl.JointAngles); // 7 Points (J1 - J7)
+        sensor.AddObservation(_pGripperControl.EndPoint); // 3 Points (x, y, z)
+        sensor.AddObservation(target.transform.position); // 3 Points (x, y, z)
     }
 
     // OnActionReceived does an action received from the policy
     public override void OnActionReceived(ActionBuffers actions)
     {
         base.OnActionReceived(actions);
+        float[] pActions = actions.ContinuousActions.Array;
+        JointPoint pActionPos = _pRobotControl.JointPos + JointPoint.FromPosition("agent_pos", pActions);
+        pActionPos.FrameCount = 1;
+        StartCoroutine(_pRobotControl.Move(pActionPos));
     }
 
     // Heuristic is called for direct action
