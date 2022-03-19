e136ff1d55412a0d3d643cb9130f62e0eff246b0
maroomir <maroomir@gmail.com>
2022-03-19 20:15:20 +0900

    Construct the pick-up spoon agent

--- Assets/Scripts/PickSpoonAgent.cs
+++ Assets/Scripts/PickSpoonAgent.cs
@@ -1,16 +1,19 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
 
-public class RobotAgent : Agent
+public class PickSpoonAgent : Agent
 {
     public GameObject robot;
     public GameObject gripper;
     public GameObject target;
     private RobotController _pRobotControl;
     private GripperController _pGripperControl;
+    private Vector3 _pInitTargetPos;
+    private Quaternion _pInitTargetRotation;
 
     // Initialize is called for ready to episode
     public override void Initialize()
@@ -20,6 +23,34 @@ public class RobotAgent : Agent
         _pGripperControl = gripper.GetComponent<GripperController>();
         _pRobotControl.ControlMode = OperationMode.Forced;
         _pGripperControl.ControlMode = OperationMode.Forced;
+        // Judge a collision
+        _pRobotControl.OnCollisionEnterEvent += OnRobotCollisionEvent;
+        // Save the initialize position
+        _pInitTargetPos = target.transform.position;
+        _pInitTargetRotation = target.transform.rotation;
+    }
+
+    private void OnRobotCollisionEvent(object sender, CollisionEventArgs e)
+    {
+        switch (e.ObjectTag)
+        {
+            case "DEAD_ZONE":
+                SetReward(-10.0F);
+                EndEpisode();
+                break;
+            case "ICE_CREAM":
+                SetReward(-10.0F);
+                EndEpisode();
+                break;
+            case "SPOON":
+                float fTargetReward = (e.SelfName == "HandE") ? 10.0F : -10.0F;
+                SetReward(fTargetReward);
+                EndEpisode();
+                break;
+            default:
+                SetReward(-0.1F);
+                break;
+        }
     }
 
     // OnEpisodeBegin is called before the episode start
@@ -27,6 +58,10 @@ public class RobotAgent : Agent
     {
         base.OnEpisodeBegin();
         Debug.Log("[AGENT] Episode Start");
+        // Target Initialize
+        target.transform.position = _pInitTargetPos;
+        target.transform.rotation = _pInitTargetRotation;
+        // Robot Initialize
         JointPoint pInitPos = JointPoint.FromPosition("Init", 135.0F, 0.0F, 90.0F, 0.0F, 90.0F, 0.0F, 0.0F);
         _pRobotControl.ForcedMove(pInitPos);
     }
@@ -68,4 +103,6 @@ public class RobotAgent : Agent
         }
         Debug.Log(pActionSegments.ToString());
     }
+    
+    
 }
